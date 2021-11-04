afd916131eb23314d0ec37e08ab417ffef94b427
lukazeljkovic <57068745+lukazeljkovic@users.noreply.github.com>
2021-11-04 19:16:56 +0100

    Update Program.cs

--- src/Build/src/ClearPluginAssemblies/Program.cs
+++ src/Build/src/ClearPluginAssemblies/Program.cs
@@ -10,44 +10,35 @@ namespace ClearPluginAssemblies
 
         protected static void Clear(string paths, IList<string> fileNames, bool saveLocalesFolders)
         {
-            foreach (var pluginPath in paths.Split(';'))
-            {
+            foreach (var pluginPath in paths.Split(';')) {
                 try
                 {
                     //komsae
                     var pluginDirectoryInfo = new DirectoryInfo(pluginPath);
                     var allDirectoryInfo = new List<DirectoryInfo> { pluginDirectoryInfo };
 
-                    if (!saveLocalesFolders)
-                        allDirectoryInfo.AddRange(pluginDirectoryInfo.GetDirectories());
+                    if (!saveLocalesFolders) allDirectoryInfo.AddRange(pluginDirectoryInfo.GetDirectories());
 
-                    foreach (var directoryInfo in allDirectoryInfo)
-                    {
-                        foreach (var fileName in fileNames)
-                        {
+                    foreach (var directoryInfo in allDirectoryInfo) {
+                        foreach (var fileName in fileNames) {
                             //delete dll file if it exists in current path
                             var dllfilePath = Path.Combine(directoryInfo.FullName, fileName + ".dll");
-                            if (File.Exists(dllfilePath))
-                                File.Delete(dllfilePath);
+                            if (File.Exists(dllfilePath))  File.Delete(dllfilePath);
                             //delete pdb file if it exists in current path
                             var pdbfilePath = Path.Combine(directoryInfo.FullName, fileName + ".pdb");
-                            if (File.Exists(pdbfilePath))
-                                File.Delete(pdbfilePath);
+                            if (File.Exists(pdbfilePath))  File.Delete(pdbfilePath);
                         }
 
-                        foreach (var fileName in FILES_TO_DELETE.Split(';'))
-                        {
+                        foreach (var fileName in FILES_TO_DELETE.Split(';')) {
                             //delete file if it exists in current path
                             var pdbfilePath = Path.Combine(directoryInfo.FullName, fileName);
-                            if (File.Exists(pdbfilePath))
-                                File.Delete(pdbfilePath);
+                            if (File.Exists(pdbfilePath))  File.Delete(pdbfilePath);
                         }
 
                         if (!directoryInfo.GetFiles().Any() && !directoryInfo.GetDirectories().Any() && !saveLocalesFolders)
                             directoryInfo.Delete(true);
                     }
-                }
-                catch
+                } catch
                 {
                     //do nothing
                 }
